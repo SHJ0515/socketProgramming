fc147b7b7d8a8a59f1943743f46b740e64e46242
송현준 <70439776+SHJ0515@users.noreply.github.com>
2024-10-31 17:42:51 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
+        ShowNaverEmailList showNaverEmailList = new ShowNaverEmailList("hoo512000@naver.com", "ejddl20011556!", "INBOX", "UNSEEN");
+
+        String[] a = showNaverEmailList.returnList();
+
+        for (String s : a) {
+            System.out.println("maidId = " + s);
+        }
+
     }
 }
